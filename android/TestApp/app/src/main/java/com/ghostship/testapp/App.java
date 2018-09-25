3e32215ebe783dcf4bf8d74145317e6ad6f4ca48
Scott Keller <scott@inspiringapps.com>
2018-09-24 22:26:34 -0600

    Removed appcenter

--- android/TestApp/app/src/main/java/com/ghostship/testapp/App.java
+++ android/TestApp/app/src/main/java/com/ghostship/testapp/App.java
@@ -1,15 +1,11 @@
 package com.ghostship.testapp;
 
 import android.app.Application;
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
 
 public final class App extends Application {
 
   @Override
   public void onCreate() {
     super.onCreate();
-    AppCenter.start(this, "7ef49956-f2c6-4b5f-bed2-974cf7ce0147", Analytics.class, Crashes.class);
   }
 }
